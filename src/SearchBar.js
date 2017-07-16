e0252c626d665473c594a0978bb7df83eb053b86
Damien <dasseya1@gmail.com>
2017-07-16 14:18:29 -0400

    filterText props added to SearchBar component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
     return (
-        <input type="search" placeholder="search" />
+        <input type="search" placeholder="search" filterText={this.props.filterText} />
     );
   }
 }
-
 export default SearchBar;
+
+SearchBar.propTypes={
+    filterText: PropTypes.string.isRequired
+}
